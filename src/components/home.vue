c8b128211b834adff034202fa99f048e397dbae5
1693647988@qq.com <1693647988@qq.com>
2020-06-21 22:51:33 +0800

    登录功能实现及删除不必要的组件

---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+    methods:{
+        //登出功能实现
+       logout(){
+           window.sessionStorage.clear();
+           this.$router.push('/login');
+       } 
+        
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import home from '../components/home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
+  //路由重定向
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect:'/login'
+  },
+  
+    {
+      path: '/login',
+      component:Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/home',
+    component:home
+},
+    
 ]
 
 const router = new VueRouter({
   routes
 })
+
+      
+     
+//挂在路由导航
+router.beforeEach((to, from, next)=>{
+  /**
+   *to:即将方问的路径      
+   *from：从哪个路径跳转而来 
+   *next：函数 
+   **/
+  
+  if (to.path === '/login')
+    return next()
+  
+  //获得token
+  const tonkerStr = window.sessionStorage.getItem('token')
+  if (!tonkerStr)
+    return next('/login')
+  next()
+  
+})
+
 
 export default router
